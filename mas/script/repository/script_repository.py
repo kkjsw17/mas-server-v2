a608edb6b042aa9d1d255181b910f00f63106150
kkjsw17 <kkjsw17@naver.com>
2023-04-14 23:35:14 +0900

    [#18] add save method to script repository

--- mas/script/repository/script_repository.py
+++ mas/script/repository/script_repository.py
@@ -1,6 +1,22 @@
 from mas.database.database_connection_manager import DatabaseConnectionManager
+from mas.script.entity.script import Script
 
 
 class ScriptRepository:
     def __init__(self, database: DatabaseConnectionManager):
         self.database = database
+
+    async def save(self, script: Script) -> Script:
+        """
+        Save the given script object to the database.
+
+        Args:
+            script (Script): The Script object to save to the database.
+
+        Returns:
+            Script: The saved User object.
+        """
+        async with self.database.get_session() as session:
+            session.add(script)
+
+        return script
