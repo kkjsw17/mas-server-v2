0601b65b8e2bdbd267129fea607e569b6457c2c1
kkjsw17 <kkjsw17@naver.com>
2023-04-22 23:36:19 +0900

    [#20] add consuming codes

--- mas/messaging/service/script_consuming_service.py
+++ mas/messaging/service/script_consuming_service.py
@@ -1,9 +1,12 @@
 import pickle
+from logging import getLogger
 
-from confluent_kafka import DeserializingConsumer
+from confluent_kafka import DeserializingConsumer, KafkaError, KafkaException
 
 from mas.utils.config import Config
 from mas.websocket.script.dto.script_response import ScriptResponse
+
+logger = getLogger()
 
 
 class ScriptConsumingService:
@@ -16,6 +19,27 @@ class ScriptConsumingService:
     def consume_script(
         self, meeting_id: int, user_id: int, script_response: ScriptResponse
     ):
-        pass
-        # script = Script(mid=meeting_id, uid=user_id, content=script_response.content)
-        # self.consumer.poll(0)
+        running = True
+
+        try:
+            self.consumer.subscribe("script")
+
+            while running:
+                msg = self.consumer.poll(timeout=1.0)
+                if msg is None:
+                    continue
+
+                if msg.error():
+                    if msg.error().code() == KafkaError._PARTITION_EOF:
+                        # End of partition event
+                        logger.error(
+                            f"{msg.topic()} [{msg.partition()} reached end at offset {msg.offset()}"
+                        )
+                    elif msg.error():
+                        raise KafkaException(msg.error())
+                else:
+                    # msg_process(msg)
+                    self.consumer.commit(asynchronous=True)
+        finally:
+            # Close down consumer to commit final offsets.
+            self.consumer.close()
