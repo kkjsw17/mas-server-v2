7b2156c62701a05c2021b1994da09f9c0fb2a9d6
kkjsw17 <kkjsw17@naver.com>
2023-06-10 22:31:12 +0900

    [#7] add middleware for logging to api server

--- mas-api/api/mas_api_application.py
+++ mas-api/api/mas_api_application.py
@@ -1,11 +1,20 @@
 import os
+from typing import Callable
 
 from api.api_initializer import APIInitializer
 from api.utils.controller_utils import create_app
 from api.utils.gunicorn_utils import MASMultiProcessingServer
+from fastapi import Request
 
 APIInitializer(os.getenv("PHASE", "local"))
 
 app = create_app("./api/api")
+
+
+@app.middleware("http")
+async def logging_middleware(request: Request, call_next: Callable):
+    response = await call_next(request)
+    return response
+
 
 MASMultiProcessingServer(app, "./config/api/gunicorn.conf.toml").run()
