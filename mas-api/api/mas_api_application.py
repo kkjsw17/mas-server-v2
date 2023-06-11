12e24949a6df1bd27b1363245af80253e9fd1642
kkjsw17 <kkjsw17@naver.com>
2023-06-11 23:32:49 +0900

    [#7] add exception handler to api server

--- mas-api/api/mas_api_application.py
+++ mas-api/api/mas_api_application.py
@@ -5,6 +5,7 @@ from api.api_initializer import APIInitializer
 from api.utils.controller_utils import create_app
 from api.utils.gunicorn_utils import MASMultiProcessingServer
 from fastapi import Request
+from fastapi.responses import JSONResponse
 
 APIInitializer(os.getenv("PHASE", "local"))
 
@@ -15,6 +16,14 @@ app = create_app("./api/api")
 async def logging_middleware(request: Request, call_next: Callable):
     response = await call_next(request)
     return response
+
+
+@app.exception_handler(Exception)
+async def exception_handler(request: Request, exc):
+    return JSONResponse(
+        status_code=500,
+        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
+    )
 
 
 MASMultiProcessingServer(app, "./config/api/gunicorn.conf.toml").run()
