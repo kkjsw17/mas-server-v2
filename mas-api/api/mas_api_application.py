4a3023110dc449c70b3e1977d7c37dda5149fa86
kkjsw17 <kkjsw17@naver.com>
2023-06-12 22:14:55 +0900

    [#7] add logging to middleware

--- mas-api/api/mas_api_application.py
+++ mas-api/api/mas_api_application.py
@@ -1,5 +1,7 @@
 import os
+from logging import getLogger
 from typing import Callable
+from uuid import uuid4
 
 from api.api_initializer import APIInitializer
 from api.utils.controller_utils import create_app
@@ -10,11 +12,15 @@ from fastapi.responses import JSONResponse
 APIInitializer(os.getenv("PHASE", "local"))
 
 app = create_app("./api/api")
+logger = getLogger()
 
 
 @app.middleware("http")
 async def logging_middleware(request: Request, call_next: Callable):
+    request_id = str(uuid4())
+    logger.info(f"[{request_id}] | REQ | {request}")
     response = await call_next(request)
+    logger.info(f"[{request_id}] | RSP | {response}")
     return response
 
 
