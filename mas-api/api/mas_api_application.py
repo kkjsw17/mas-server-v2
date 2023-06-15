6f4d75d80219b119f134a55182e14e2a84764896
kkjsw17 <kkjsw17@naver.com>
2023-06-15 11:35:10 +0900

    [#7] separate exception handler

--- mas-api/api/mas_api_application.py
+++ mas-api/api/mas_api_application.py
@@ -1,9 +1,7 @@
 import os
 
-from fastapi import Request
-from fastapi.responses import JSONResponse
-
 from api.api_initializer import APIInitializer
+from api.exception_handler.global_exception_handler import global_exception_handler
 from api.middleware.logging_middleware import LoggingMiddleware
 from api.utils.controller_utils import create_app
 from api.utils.gunicorn_utils import MASMultiProcessingServer
@@ -11,15 +9,11 @@ from api.utils.gunicorn_utils import MASMultiProcessingServer
 APIInitializer(os.getenv("PHASE", "local"))
 
 app = create_app(".")
+
+# add middlewares
 app.add_middleware(LoggingMiddleware)
 
-
-@app.exception_handler(Exception)
-async def exception_handler(request: Request, exc):
-    return JSONResponse(
-        status_code=500,
-        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
-    )
-
+# add exception handlers
+app.add_exception_handler(Exception, global_exception_handler)
 
 MASMultiProcessingServer(app, "./config/api/gunicorn.conf.toml").run()
