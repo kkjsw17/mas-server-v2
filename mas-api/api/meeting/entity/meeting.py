45dec1ec7ee2a05677264ce3133b8b9928519e56
kkjsw17 <kkjsw17@naver.com>
2023-07-12 17:38:50 +0900

    [#6] add meeting entity

---
+++ mas-api/api/meeting/entity/meeting.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from api.common.database.database_connection_manager import Base
+from api.utils.datetime_utils import get_now_datetime_by_timezone
+
+
+class Meeting(Base):
+    __tablename__ = "Meeting"
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True, autoincrement=True)
+    host_id: Mapped[int]
+    title: Mapped[str]
+    code: Mapped[str]
+    capacity: Mapped[int]
+    created_at: Mapped[datetime] = mapped_column(
+        default_factory=get_now_datetime_by_timezone
+    )
+    modified_at: Mapped[datetime] = mapped_column(
+        default_factory=get_now_datetime_by_timezone
+    )
